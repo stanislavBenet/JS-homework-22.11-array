f603cf108179b3451216cc86755c05e71f0d2a22
stanislavBenet <stanislav.benet@gmail.com>
2022-11-23 09:26:58 +0200

    Add edit homework 22.11

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 // 1) Знайти середнє арифметичне значення усіх переданих аргументів, якщо аргументів не має - повертати null
 
-// function middleValueArray() {
-//   let summ = 0;
-//   if (arguments.length > 0) {
-//     for (let i = 0; i < arguments.length; i++) {
-//       summ += arguments[i];
-//     }
-//     return summ / arguments.length;
-//   }
-//   return null;
-// }
+function averageArray() {
+  let summ = 0;
+  if (arguments.length > 0) {
+    for (let i = 0; i < arguments.length; i++) {
+      summ += arguments[i];
+    }
+    return summ / arguments.length;
+  }
+  return null;
+}
 
-// middleValueArray();
+console.log(averageArray(1, 2, 4, 5, 6, 7, 3, 4));
 
-// // 2) Замінити усі максимальні значення в масиві на значення 0
+// 2) Замінити усі максимальні значення в масиві на значення 0
 
-// function changeAllMaxElementToNull(arr) {
-//   let max = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > max) {
-//       max = arr[i];
-//     }
-//   }
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === max) {
-//       arr[i] = 0;
-//     }
-//   }
-//   return arr;
-// }
-let middleval = 0;
-let array1 = [2, 4, 5, 11, 25, 11, 25, 1, 2, 4, 5, 25];
-let middle = array1.reduce(middleval);
+function changeAllMaxElementToZero(arr) {
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === max) {
+      arr[i] = 0;
+    }
+  }
+  return arr;
+}
+const array = [2,4,5,6,7,8,8,8,8,0,7,8,8]
+console.log(changeAllMaxElementToZero(array));
